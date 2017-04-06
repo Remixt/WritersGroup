3991320d2a032d456cd12622bd0502eccdc23f9b
Satyanarayana <chivukula181@gmail.com>
2017-04-05 19:10:57 -0500

    Flow Changed

---
+++ app/src/main/java/csce/unt/writersgroup/model/Group.java
@@ -0,0 +1,14 @@
+package csce.unt.writersgroup.model;
+
+import java.io.Serializable;
+
+/**
+ * Created by Satyanarayana on 4/5/2017.
+ */
+public class Group implements Serializable {
+
+    private String groupId;
+    private String users;
+    private String anchors;
+
+    }
