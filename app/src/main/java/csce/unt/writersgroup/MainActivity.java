37bfbff2906aaa8c14ca96b34a01b0b335b0920e
Satyanarayana <chivukula181@gmail.com>
2017-04-25 12:11:12 -0500

    Logout Added

--- app/src/main/java/csce/unt/writersgroup/MainActivity.java
+++ app/src/main/java/csce/unt/writersgroup/MainActivity.java
@@ -1,5 +1,6 @@
 package csce.unt.writersgroup;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -7,12 +8,10 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.text.Layout;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -156,6 +155,10 @@ public class MainActivity extends AppCompatActivity
         else if (id == R.id.nav_send)
         {
 
+        }
+        else if (id == R.id.nav_logout)
+        {
+            startActivity(new Intent(MainActivity.this, LoginActivity.class));
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
